868bdc991f1fd133a4dd6439c3af8686505f9e1f
yi-jou-huang <df467289@gmail.com>
2024-04-29 12:43:57 +0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+﻿const Home = () => {
+    const handleClick = (e)=>{
+        console.log("Click me!", e)
+    }
+    const handleClicktwo = ()=>{
+        console.log("Click me only first render")
+    }
+    const handleClickAgain = (e)=>{
+        console.log(e.type, e.target, e.target.innerHTML)
+    }
+    const name ="lulu"
+    return (  
+        <div className="home">
+            <h2>Homepage</h2>
+            <button onClick={ handleClick }>Click me</button>
+            <button onClick={ handleClicktwo() }>Click me only first render</button>
+            <button onClick={(e)=>handleClickAgain(e)}> { name } </button>
+        </div>
+    );
+}
+ 
+export default Home;
