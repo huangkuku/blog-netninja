c1a4fc4f9a2a646bdcdd812f63397d8aa42d0915
huang1993 <150344345+huangkuku@users.noreply.github.com>
2024-04-30 14:22:45 +0800

    edit new Home.js and BlogList.js

---
+++ src/BlogList.js
@@ -0,0 +1,22 @@
+﻿const BlogList = ({blogs, title, handleDelete}) => {  
+    // BlogList=(props)=>, props is a Obj, {blogs: Array(3), title: " All Blogs ~"} blogs裡的的array
+    // const blogs = props.blogs  // Array, [{...,id:1}, {...,id:2},{...,id:3}]
+    // const title= props.title  // title: All Blogs ~
+    // const arr = [1,2] const str ="string" const mer = {arr,str} // console.log(mer) {arr: Array(2), str: 'string'}
+    
+    return (  
+        <div className="blog-list">
+            <h2> { title } </h2>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key= { blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <button onClick= {()=> handleDelete(blog.id)}> Delete Blog</button>
+                </div>
+                
+            ))}
+        </div>
+    );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 ﻿import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -6,18 +7,21 @@ const Home = () => {
         {title: 'Welcome party !', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         {title: 'Web dev top tips ?', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
-    // const what = useState(1) ab= what[0] //1 setAb= what[1] // ƒ dispatchSetState(fiber, queue, action)...
+    // 原本是const [ab, setAb] = useState(1)我們來看useState(),用what代替[ab, setAb] const what = useState(1), ab= what[0] //1 ,setAb= what[1] // ƒ dispatchSetState(fiber, queue, action)...
+    const handleDelete= (id) =>{  // 這裡的id === BlogList.js的 blog.id
+        const result = blogs.filter((blog)=> blog.id !== id)
+        // blogs.filter((blog)=> blog.id === id) 符合條件為 blog.id 等於 id的,篩選出,其他都不要
+        // blogs.filter((blog)=> blog.id !== id) 符合條件為 blog.id不等於id的,篩選出,其他都不要
+        setBlogs(result)
+    }
+
     // .map( ()=> () ) 為什麼" ()=> "的 右邊是"()" 因為要用template呈現 就像return () 
     return (  
         <div className="home">
-            {blogs.map((blog)=>(
-                <div className="blog-preview" key= { blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-                
-            ))}
-            
+            <BlogList blogs={ blogs } title="All Blogs ~" handleDelete={handleDelete} />
+            {/* Home.js的 < BlogList blogs={ blogs } title="All Blogs ~"/> 
+            BlogList.js的 const BlogList = (props) => {... 
+            BlogList.js的 props就是 blogs={ blogs } title="All Blogs ~" ,props把它包成一個Obj*/}
         </div>
     );
 }
