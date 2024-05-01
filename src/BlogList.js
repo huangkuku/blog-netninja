dc56ce4a880654489409373b208e354adaf2766d
huang1993 <150344345+huangkuku@users.noreply.github.com>
2024-05-01 09:59:39 +0800

    src folder update Home.js BlogList.js index.css

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,13 @@
-﻿const BlogList = ({blogs, title, handleDelete}) => {  
+﻿const BlogList = ({blogs, title}) => {  
     // BlogList=(props)=>, props is a Obj, {blogs: Array(3), title: " All Blogs ~", handleDelete= (id)=>{...}} blogs裡的的array
-    // const arr = [1,2] const str ="string" const mer = {arr,str} // console.log(mer) {arr: Array(2), str: 'string'}
 
     return (  
         <div className="blog-list">
             <h2> { title } </h2>
-            {blogs.map((blog)=>(
+            {blogs.map( blog =>(
                 <div className="blog-preview" key= { blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
-                    <button onClick= {()=> handleDelete(blog.id)}> Delete Blog</button>
                 </div>
                 
             ))}
--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,24 @@
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website ~ ~', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        {title: 'Welcome party !', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        {title: 'Web dev top tips ?', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-    const handleDelete= (id) =>{  // 這裡的id === BlogList.js的 blog.id
-        const result = blogs.filter((blog)=> blog.id !== id)
-        // blogs.filter((blog)=> blog.id === id) 符合條件為 blog.id 等於 id的,篩選出,其他都不要
-        // blogs.filter((blog)=> blog.id !== id) 符合條件為 blog.id不等於id的,篩選出,其他都不要
-        setBlogs(result)
-    }
-    const [name, setName] = useState("Linda")
+    const [blogs, setBlogs] = useState(null)
+
     useEffect( ()=>{
         console.log("useEffect re-render")
-        console.log(name)
-    }, [name]) // [條件], re-render條件(跑useEffect()) 如果為空的[],則只有第一次渲染，才會跑useEffect() [name],name改變時跑useEffect()
+        fetch('http://localhost:8000/blogs')
+        .then(res =>{
+            return res.json()
+        })
+        .then(data =>{
+            console.log(data)
+            setBlogs(data)
+        })
+    }, []) // [條件], re-render條件(跑useEffect()) 
 
     return (  
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs ~" handleDelete={handleDelete} />
-            {/* BlogList.js的 props就是 <BlogList>的 blogs={ blogs } title="All Blogs ~"handleDelete={handleDelete} ,props把它包成一個Obj*/}
-            <button onClick={()=> setName("Amy")}>change name</button>
-            <p>{ name }</p>
+            {blogs && <BlogList blogs={ blogs } title="All Blogs ~" />}
+            
         </div>
     );
 }
