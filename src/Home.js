ead33e5814941c8af9de985ce1ec44a93d8675ac
huang1993 <150344345+huangkuku@users.noreply.github.com>
2024-05-01 15:30:11 +0800

    new file: useFetch.js,  and Home.js add new note

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,18 @@
 ﻿import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFecth";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-
-    useEffect( ()=>{
-        console.log("useEffect re-render")
-        fetch('http://localhost:8000/blogs')
-        .then(res =>{
-            return res.json()
-        })
-        .then(data =>{
-            console.log(data)
-            setBlogs(data)
-        })
-    }, []) // [條件], re-render條件(跑useEffect()) 
-
+    // const [one, setOne]= useState(1); one=1, setOne= func()/改變後的one; useState is [初始值,改變後的值]
+    const { data: blogs, isPending, error }= useFetch('http://localhost:8000/blogs') 
+    // useFetch() is Obj:{東西1,東西2,...}, useFetch(url) url是http... 之後使用useFetch只要換url就好
+    console.log("data: blogs內容", blogs) // null; array(2) [{…}, {…}]
     return (  
         <div className="home">
-            {blogs && <BlogList blogs={ blogs } title="All Blogs ~" />}
-            
+            { isPending && <p>Loading...</p> } {/* true, 執行&&右邊的code; 當fetch server完成時變成false,不執行&&右邊的() */}
+            { error && <div>{ error }</div> } {/* 一開始為null, 若有改變setError(error.message)，則執行右邊的code */}
+            { blogs && <BlogList blogs={ blogs } title="All Blogs ~"/> }
         </div>
     );
-}
- 
+} 
 export default Home;
---
+++ src/useFecth.js
@@ -0,0 +1,33 @@
+﻿import { useState, useEffect } from "react"
+
+const useFetch = (url) => {  // useFetch 節省你寫isPending error 重複的code 那blogs怎麼辦呢 用data替換; 取得url值
+    const [data, setData] = useState(null) // useFetch的data可以是blogs 、tags、categories...等
+    const [isPending, setIsPending] = useState(true)// pending, loading
+    const [error, setError] = useState(null)   // 用來儲存error訊息 
+    console.log("data", data) // null; array(2) [{…}, {…}]
+    useEffect( ()=>{
+        setTimeout(()=>{  // seTimeout( func(), unit:ms)
+            fetch(url) // url 用在這裡
+            .then(res =>{
+                if(!res.ok){ // error URL: res.ok= false, correct URL:res.ok= true
+                    throw Error("couldn't fetch the data for that resource.")
+                }
+                return res.json()
+            })
+            .then(d =>{
+                console.log("d:",d) // // null; array(2) [{…}, {…}]
+                setData(d)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch( error =>{
+                setIsPending(false)
+                setError(error.message) // console.log(error.message), error.message存到setError()內
+            })
+        },1000)
+    }, []) // [條件], re-render條件(跑useEffect()) 
+
+    return {data, isPending, error} // 使用 useFetch(), return 這些東西 可以是arr, Obj 
+}
+ 
+export default useFetch;
